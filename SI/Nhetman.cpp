2ff32d341d5f87f0a7e13f7248bc0f7b01e9a17a
Sebastian Ratańczuk <46678414+azaney@users.noreply.github.com>
2020-10-18 20:21:33 +0200

    Update Nhetman.cpp

--- SI/Nhetman.cpp
+++ SI/Nhetman.cpp
@@ -1,112 +1,197 @@
 ﻿#include <iostream>
 #include <list>
+#include <chrono>
 
 using namespace std;
 
-class Pole {
-public:
-    int* hetmani;
-    int rozmiar;
-    int licznik;
+int rozmiar;
 
-    Pole() { licznik = 0; rozmiar = 0; hetmani = NULL; }
+int licznik_wygenerowanych = 0;
+int licznik_sprawdzonych = 0;
 
-    Pole(int n) { 
+int* inicjalizacja(int n)
+{ 
+    rozmiar = n;
+    int *hetmani = new int[n]; 
+    for (int i = 0; i < rozmiar; i++)
+        hetmani[i] = -1;
+    return hetmani;
+}
 
-        rozmiar = n;
-        hetmani = new int[n]; 
+int* kopiujHetmanow(int* hetmani)
+{    
+    int *nowy = new int[rozmiar];
+    for (int i = 0; i < rozmiar; i++)    
+        nowy[i] = hetmani[i];    
+    return nowy;
+}
 
-        for (int i = 0; i < rozmiar; i++)
-            hetmani[i] = -1;
-    }
+void pokaz(int* hetmani, bool matrix = false)
+{        
+    for (int i = 0; i < rozmiar; i++)        
+        cout << "x: " << i << " y: " << hetmani[i] << endl;        
+    cout << endl;
 
-    Pole(const Pole &a)
+    if (matrix)
     {
-        rozmiar = a.rozmiar;
-        licznik = a.licznik;
-        hetmani = new int[rozmiar];
         for (int i = 0; i < rozmiar; i++)
         {
-            hetmani[i] = a.hetmani[i];
-        }
-    }
-
-    void show()
-    {        
-        for (int i = 0; i < rozmiar; i++)        
-            cout << "x: " << i << " y:" << hetmani[i] << endl;
-        
-        cout << endl;
-    }
-
-    bool dodajHetmana(int x, int y)
-    {       
-        if (hetmani[x] == -1)
-        {
-            for (int i = 0; i < rozmiar; i++)
-            {
-                if (y == hetmani[i])
-                    return false;
-
-                if (hetmani[i] != -1)
-                    if (abs(i - x) == abs(hetmani[i] - y))
-                        return false;
-            }
-
-            hetmani[x] = y;
-            licznik++;
-            return true;
-        }
-        return false;              
-    }  
-
-    list<int*> BruteForce()
-    {
-        list<int*> toRet;
-        for (int i = 0; i < rozmiar; i++)        
             for (int j = 0; j < rozmiar; j++)
             {
-                Pole nowePole(*this);
-                if (nowePole.dodajHetmana(i, j))                
-                    toRet.push_back(nowePole.hetmani);                
-            }        
+                if (hetmani[i] != j)
+                    cout << "0 ";
+                else
+                    cout << "1 ";
+            }
+            cout << endl;
+        }
+    }       
+}
 
-        return toRet;
-    }
-
-    list<int*> Separacja()
+bool dodajHetmana(int x, int y, int* hetmani)
+{       
+    if (hetmani[x] == -1)
     {
-        list<int*> toRet;
-        int i = 0;
-        for (i = 0; i < rozmiar; i++)        
-            if (hetmani[i] == -1)
-                break;
-        
-        for (int j = 0; j < rozmiar; j++)
+        for (int i = 0; i < rozmiar; i++)
         {
-            Pole nowePole(*this);
-            if (nowePole.dodajHetmana(i, j))
-                toRet.push_back(nowePole.hetmani);
+            if (y == hetmani[i])
+                return false;
+
+            if (hetmani[i] != -1)
+                if (abs(i - x) == abs(hetmani[i] - y))
+                    return false;
         }
 
-        return toRet;
+        hetmani[x] = y;
+        return true;
     }
+    return false;              
+}  
 
-    list<int*> Inteligentne(int* hetmani)
+list<int*> BruteForce(int* hetmani)
+{
+    list<int*> toRet;
+    for (int i = 0; i < rozmiar; i++)        
+        for (int j = 0; j < rozmiar; j++)
+        {
+            int* nowiHetmani = kopiujHetmanow(hetmani);
+            if (dodajHetmana(i, j,nowiHetmani))                
+                toRet.push_back(nowiHetmani);                
+        }        
+
+    return toRet;
+}
+
+list<int*> Separacja(int* hetmani)
+{
+    list<int*> toRet;
+    int i = 0;
+    for (i = 0; i < rozmiar; i++)        
+        if (hetmani[i] == -1)
+            break;
+        
+    for (int j = 0; j < rozmiar; j++)
     {
-
+        int* nowiHetmani = kopiujHetmanow(hetmani);
+        if (dodajHetmana(i, j, nowiHetmani))
+            toRet.push_back(nowiHetmani);
     }
-};
+
+    return toRet;
+}
+
+int licznosc(int* hetmani)
+{
+    int suma = 0;
+    for (int i = 0; i < rozmiar; i++)    
+        if (hetmani[i] != -1)
+            suma++;    
+    return suma;
+}
+
+void BFS(int rozmiar, bool szybko = true)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    list<int*> kolejka;
+    kolejka.push_back(inicjalizacja(rozmiar));
+    
+    while (!kolejka.empty())
+    {   
+        licznik_sprawdzonych++;
+
+        int* badanyStan = kolejka.front();
+        kolejka.pop_front();
+        if (licznosc(badanyStan) == rozmiar)
+        {
+            pokaz(badanyStan);
+            break;
+        }
+
+        list<int*>noweStany;
+
+        if(szybko)
+            noweStany = Separacja(badanyStan);
+        else
+            noweStany = BruteForce(badanyStan);
+
+        for (int* var : noweStany)
+        {
+            kolejka.push_back(var);
+            licznik_wygenerowanych++;
+        }
+    }
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+    std::cout << "Czas szukania rozwiazania: " << duration.count() / 1000000.0 << " sekund.\n";
+    cout << "Ilosc stanow wygenerowanych: " << licznik_wygenerowanych << endl;
+    cout << "Ilosc stanow sprawdzonych: " << licznik_sprawdzonych << endl;
+}
+
+void DFS(int rozmiar, bool szybko = true)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    list<int*> kolejka;
+    kolejka.push_back(inicjalizacja(rozmiar));
+
+    while (!kolejka.empty())
+    {
+        licznik_sprawdzonych++;
+
+        int* badanyStan = kolejka.back();
+        kolejka.pop_back();
+        if (licznosc(badanyStan) == rozmiar)
+        {
+            pokaz(badanyStan);
+            break;
+        }
+
+        list<int*>noweStany;
+
+        if (szybko)
+            noweStany = Separacja(badanyStan);
+        else
+            noweStany = BruteForce(badanyStan);
+
+        for (int* var : noweStany)
+        {
+            kolejka.push_back(var);
+            licznik_wygenerowanych++;
+        }
+    }
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+    std::cout << "Czas szukania rozwiazania: " << duration.count() / 1000000.0 << " sekund.\n";
+    cout << "Ilosc stanow wygenerowanych: " << licznik_wygenerowanych << endl;
+    cout << "Ilosc stanow sprawdzonych: " << licznik_sprawdzonych << endl;
+}
 
 int main()
 {
-    Pole plansza(4);   
-    plansza.dodajHetmana(0, 0);
-    list<int*> k = plansza.Separacja();
-
-    for (int* var : k) {
-        for (int i = 0; i < 4; i++)
-            cout << "x: " << i << " y:" << var[i] << endl;
-        cout << endl;
-    }
+    DFS(20);
 }
